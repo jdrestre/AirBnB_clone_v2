42bf0623e6ca239b27ff3104d8377657b6ec7de0
jdrestre <jdrestre79@gmail.com>
2020-04-12 11:02:17 -0500

    Add correctio for file wiht do_deploy prototype - Erase prototype do_pack - organized identation pep8 - orgnaized folders versions and web_static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,37 +11,24 @@ env.hosts = ['34.74.80.43', '54.89.185.132']
 env.user = 'ubuntu'
 
 
-def do_pack():
-    """ Generates a .tgz tar gzipped archive from the contents
-        of the web_static folder of your AirBnB Clone repo
-    """
-    dt = datetime.utcnow()
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year, dt.month,
-                                                         dt.day, dt.hour,
-                                                         dt.minute, dt.second)
-    if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
-            return None
-    if local("tar -cvzf {} web_static".format(file)).failed is True:
-        return None
-    return file
-
-
 def do_deploy(archive_path):
     """Distributes an archive to your web servers
     """
-    if os.path.isfile(archive_path) is False:
+if os.path.isfile(archive_path) is False:
         return False
     file = archive_path.split("/")[-1]
     name = file.split(".")[0]
 
     if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".format(name)).failed is True:
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
         return False
-    if run("mkdir -p /data/web_static/releases/{}/".format(name)).failed is True:
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
         return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file, name)).failed is True:
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
         return False
     if run("rm /tmp/{}".format(file)).failed is True:
         return False
