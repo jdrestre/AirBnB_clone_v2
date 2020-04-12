15fc2320c07f7390aec02fb0e991a12b97f3c7c3
jdrestre <jdrestre79@gmail.com>
2020-04-12 11:06:08 -0500

    Add again prototype do_pack remove env user ubuntu is overscript

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,7 +8,22 @@ from datetime import datetime
 import os.path
 
 env.hosts = ['34.74.80.43', '54.89.185.132']
-env.user = 'ubuntu'
+
+
+def do_pack():
+    """ Generates a .tgz tar gzipped archive from the contents
+        of the web_static folder of your AirBnB Clone repo
+    """
+    dt = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year, dt.month,
+                                                         dt.day, dt.hour,
+                                                         dt.minute, dt.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
+        return None
+    return file
 
 
 def do_deploy(archive_path):
