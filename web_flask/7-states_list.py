faa2e06b69c700688fbc938e4d8c52f28af8482e
jdrestre <jdrestre79@gmail.com>
2020-04-23 10:01:18 -0500

    Update identation html file Remove comment line about import sql to have some data

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -13,7 +13,6 @@ Routes:
         UL tag: with the list of all State objects present in DBStorage
         sorted by name (A->Z) tip
             LI tag: description of one State: <state.id>: <B><state.name></B>
-Import this 7-dump to have some data
 You must use the option strict_slashes=False in your route definition
 """
 from flask import Flask
